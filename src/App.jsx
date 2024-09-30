7db05fdd7a367d8e4f039458fb9ada6b29e25d0e
samir <samir.elanany@microengineering.ca>
2024-09-30 20:40:46 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Base from "./components/Base";
+import Toppings from "./components/Toppings";
+import Order from "./components/Order";
+
+function App() {
+  const [pizza, setPizza] = useState({ base: "", toppings: [] });
+
+  const addBase = (base) => {
+    setPizza({ ...pizza, base });
+  };
+
+  const addTopping = (topping) => {
+    let newToppings;
+    if (!pizza.toppings.includes(topping)) {
+      newToppings = [...pizza.toppings, topping];
+    } else {
+      newToppings = pizza.toppings.filter((item) => item !== topping);
+    }
+    setPizza({ ...pizza, toppings: newToppings });
+  };
+
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/base"
+          element={<Base addBase={addBase} pizza={pizza} />}
+        />
+        <Route
+          path="/toppings"
+          element={<Toppings addTopping={addTopping} pizza={pizza} />}
+        />
+        <Route path="/order" element={<Order pizza={pizza} />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
